3f0dd9aa0f410b61bcfef1b64f8ce4f62166151d
Edgar Nova <ragnarok540@gmail.com>
2026-07-30 19:13:19 -0500

    learn prolog now ch4 end

--- lpn/ch4.pl
+++ lpn/ch4.pl
@@ -40,3 +40,36 @@ twice(A, B) :-
 % twice([a,4,buggle],X).
 % twice([1,2,1,1],X).
 % twice(X, [1,1,2,2,3,3]).
+
+combine1([], [], []).
+combine1(Left, Right, Combined) :-
+  Left = [L|T1],
+  Right = [R|T2],
+  Combined = [L, R|T3],
+  combine1(T1, T2, T3).
+
+% combine1([a,b,c],[1,2,3],X).
+% combine1([f,b,yip,yup],[glu,gla,gli,glo],Result).
+% combine1(Left,Right,[3,c,2,b,1,a]).
+
+combine2([], [], []).
+combine2(Left, Right, Combined) :-
+  Left = [L|T1],
+  Right = [R|T2],
+  Combined = [[L, R]|T3],
+  combine2(T1, T2, T3).
+
+% combine2([a,b,c],[1,2,3],X).
+% combine2([f,b,yip,yup],[glu,gla,gli,glo],Result).
+% combine2(Left,Right,[[3,c],[2,b],[1,a]]).
+
+combine3([], [], []).
+combine3(Left, Right, Combined) :-
+  Left = [L|T1],
+  Right = [R|T2],
+  Combined = [j(L, R)|T3],
+  combine3(T1, T2, T3).
+
+% combine3([a,b,c],[1,2,3],X).
+% combine3([f,b,yip,yup],[glu,gla,gli,glo],Result).
+% combine3(Left,Right,[j(3,c),j(2,b),j(1,a)]).
