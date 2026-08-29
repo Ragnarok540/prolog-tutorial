8998216e92c35b32116a3bad48290c71fd8a7324
Edgar Nova <ragnarok540@gmail.com>
2026-08-29 15:31:13 -0500

    learn prolog now ch8 end

--- lpn/ch8.pl
+++ lpn/ch8.pl
@@ -1,7 +1,10 @@
 s --> np(subject), vp.
 s --> np(subject, plural), vp(plural).
+s --> np(subject), pp.
+s --> np(subject), vp, pp.
 
 np(_) --> det, n.
+np(_) --> det, adj, n.
 np(X) --> pro(X).
 np(subject, plural) --> det, n(plural).
 
@@ -20,6 +23,12 @@ v(plural) --> [Word], {lex(Word, v, plural)}.
 pro(subject) --> [Word], {lex(Word, ps)}.
 pro(object) --> [Word], {lex(Word, po)}.
 
+adj --> [Word], {lex(Word, adj)}.
+
+p --> [Word], {lex(Word, p)}.
+
+pp --> p, np(_).
+
 lex(the, det).
 lex(a, det).
 lex(woman, n).
@@ -32,6 +41,13 @@ lex(he, ps).
 lex(she, ps).
 lex(him, po).
 lex(her, po).
+lex(small, adj).
+lex(big, adj).
+lex(in, p).
+lex(on, p).
+lex(from, p).
+lex(of, p).
+lex(at, p).
 
 lex(men, n, plural).
 lex(eat, v, plural).
@@ -40,3 +56,6 @@ lex(know, v, plural).
 % s([her,shoots,she],[]).
 % s([she,shoots,him],[]).
 % s([the,man,eats],[]).
+% s([the,big,man,eats],[]).
+% s([the,man,in,the,woman],[]). 
+% s([the,man,eats,on,the,woman],[]).
