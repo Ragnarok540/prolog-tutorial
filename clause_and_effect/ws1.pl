6df42ff058dc9252e505e026b7a2575b9b7e68f8
Edgar Nova <ragnarok540@gmail.com>
2026-09-12 12:58:01 -0500

    clause and effect - ws 1 & 2

---
+++ clause_and_effect/ws1.pl
@@ -0,0 +1,21 @@
+% Worksheet 1: Party Pairs
+
+male(bertram).
+male(percival).
+female(lucinda).
+female(camilla).
+
+pair(X, Y) :-
+    male(X),
+    female(Y).
+
+% pair(percival, X).
+% pair(apollo, daphne).
+% pair(camilla, X).
+% pair(X, lucinda).
+% pair(X, X).
+% pair(bertram, lucinda).
+% pair(X, fido).
+% pair(X, Y).
+
+% ^ Cartesian Product
