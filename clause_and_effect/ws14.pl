8ea7c6b4abc1b0836e3586fdcb6407c450ddee6d
Edgar Nova <ragnarok540@gmail.com>
2026-09-20 07:59:35 -0500

    clause and effect - ws 14

---
+++ clause_and_effect/ws14.pl
@@ -0,0 +1,26 @@
+% Worksheet 14: Partial Maps with a ParaDleter
+
+reduce([X|T], X, T).
+reduce([H|T], X, [H|L]) :-
+    reduce(T, X, L).
+
+a(g, h).
+a(d, a).
+a(g, d).
+a(e, d).
+a(h, f).
+a(e, f).
+a(a, e).
+a(a, b).
+a(b, f).
+a(b, c).
+a(f, c).
+
+path(X, X, _, end).
+path(X, Y, L, Route) :-
+    a(X, Z),
+    reduce(L, Z, L1),
+    path(Z, Y, L1, Route0),
+    Route = go(X, Z, Route0).
+
+% path(a, b, [a,b,c,d,e,f,g,h], R).
